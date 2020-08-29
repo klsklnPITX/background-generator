b710a2f9cf26a381c0bf2b192dd42df5a1d2490f
klsklnPITX <67044656+klsklnPITX@users.noreply.github.com>
2020-08-29 10:24:06 +0200

    Added eventListener, removed onInput

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("background");
+var cssInfo = document.querySelector("h4")
 
 function changeColor() {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    cssInfo.textContent = body.style.background;
 }
 
-color1.addEventListener("input", changeColor());    
+color1.addEventListener("input", changeColor);
+color2.addEventListener("input", changeColor);
+
